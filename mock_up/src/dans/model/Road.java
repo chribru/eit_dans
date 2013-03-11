f8e0a4aeeea1489ff3c3bb303465bf5a169007da
Christer Bru <christer@regnbue.(none)>
2013-03-11 20:04:47 +0100

    Added a MainWindow, got started on htmlify

--- mock_up/src/dans/model/Road.java
+++ mock_up/src/dans/model/Road.java
@@ -4,6 +4,19 @@ public class Road {
 	private RoadStatus status;
 	private String idCode;
 	private String name;
+	
+	public Road() {
+		this.name = "";
+		this.idCode = "";
+		this.status = RoadStatus.OPEN;
+	}
+		
+	public Road(String idCode, String name, RoadStatus status) {
+		this.idCode = idCode;
+		this.name = name;
+		this.status = status;
+	}
+
 	public RoadStatus getStatus() {
 		return status;
 	}
--- mock_up/src/dans/view/Sign.java
+++ mock_up/src/dans/view/Sign.java
@@ -1,6 +1,7 @@
 package dans.view;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,18 +11,33 @@ import dans.model.Warning;
 public class Sign extends JPanel {
 	private static final long serialVersionUID = 391385783177984311L;
 	private JLabel htmlLabel;
-	public Sign(){
+	private int capacity;
+	
+	public Sign(int capacity){
+		this.capacity = capacity;
+		setBackground(Color.BLACK);
 		htmlLabel = new JLabel();
-		htmlLabel.setBackground(Color.BLACK);
+		htmlLabel.setForeground(Color.YELLOW);
+		htmlLabel.setFont(htmlLabel.getFont().deriveFont(Font.PLAIN, 42f));
+//		htmlLabel.setText("<html>Be <b>bold</b>!</html>");
 		add(htmlLabel);
-		
 	}
+	
+	public Sign() {
+		this(2);
+	}
+
+	
 	public void setWarning(Warning warning){
 		htmlLabel.setText(htmlify(warning));
 	}
 	
 	private static String htmlify(Warning warning){
-		//TODO: Translation into HTML.
-		return null;
+		StringBuilder builder = new StringBuilder();
+		builder.append("<html><center>");
+		builder.append(warning.getRoad().getIdCode()+ "<br>");
+		builder.append(warning.getRoad().getName()+ "<br>");
+		builder.append("</center></html>");
+		return builder.toString();
 	}
 }
